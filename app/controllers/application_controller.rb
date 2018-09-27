1eaae097b2cfd315c702175c93a20475289f1483
Marcus Paiva <marcuspaiva2014@gmail.com>
2018-09-27 18:43:05 +0000

    first commit bug fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  layout 'application'
+  def index
+      render template: 'index/index.html.erb'
+      #render html: 'olá'
+  end
+  
 end
