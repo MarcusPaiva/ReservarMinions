89ea3086af0696932b91ba903dcc442aa09dba79
Marcus Paiva <marcuspaiva2014@gmail.com>
2018-09-28 03:25:52 +0000

    Improved form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   layout 'application'
+  @status = ""
+  # default route
   def index
+      @status = ""
       render template: 'index/index.html.erb'
       #render html: 'olá'
+  end
+  
+  def reserved
+    if params
+      @status = "Reservado"
+    else
+      @status = ""
+    end
+    render template: 'index/index.html.erb'
   end
   
 end
