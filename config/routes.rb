b0f0fe155d9f681b8cf8a7ed35707a429e9e526a
Marcus Paiva <marcuspaiva2014@gmail.com>
2018-09-28 03:27:20 +0000

    Add new route: reserved

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#index'
-  match '/index/index' => 'application#index', via: :post
+  match '/reserved' => 'application#reserved', via: :get
+  match '/reserved' => 'application#reserved', via: :post
 end
